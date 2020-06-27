2c6e41159d07aa2fc17be12fa4a26c3eb4a6aa13
ak <ak@thusspakeak.com>
2020-06-27 17:15:44 +0100

    added BFGSMinimimum

--- TestOptimise.js
+++ TestOptimise.js
@@ -3,6 +3,7 @@
 (function() {
  var all = [];
  all.push('TestOptimise/TestAnnealMinimum.js');
+ all.push('TestOptimise/TestBFGSMinimum.js');
  all.push('TestOptimise/TestBisectMinimum.js');
  all.push('TestOptimise/TestBlindfoldMinimum.js');
  all.push('TestOptimise/TestGeneticMinimum.js');
