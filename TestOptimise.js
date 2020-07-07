668bbf25906e332918593f6eddf3e2905f3c8061
ak <ak@thusspakeak.com>
2020-07-07 15:27:38 +0100

    aligning text

--- TestOptimise.js
+++ TestOptimise.js
@@ -3,7 +3,7 @@
 (function() {
  var all = [];
  all.push('TestOptimise/TestAnnealMinimum.js');
-  all.push('TestOptimise/TestBisectMinimum.js');
+ all.push('TestOptimise/TestBisectMinimum.js');
  all.push('TestOptimise/TestBlindfoldMinimum.js');
  all.push('TestOptimise/TestGeneticMinimum.js');
  all.push('TestOptimise/TestGoldenMinimum.js');
