8e06e18c5856c458a6cc2c81a9000e6657e614f4
ak <ak@thusspakeak.com>
2020-07-31 22:37:00 +0100

    adding max steps

--- TestCalculus/TestFehlbergODE.js
+++ TestCalculus/TestFehlbergODE.js
@@ -47,6 +47,7 @@
    invalidArguments.add('non-number c element', function(){try{ak.fehlbergODE(Math.exp, 0.1, 0.1, 0.2, 5, a, b0, b1, [0.5,0.5,'a']);} catch(e){return true;} return false;});
    invalidArguments.add('zero c element', function(){try{ak.fehlbergODE(Math.exp, 0.1, 0.1, 0.2, 5, a, b0, b1, [0.5,0.5,0]);} catch(e){return true;} return false;});
    invalidArguments.add('NaN c element', function(){try{ak.fehlbergODE(Math.exp, 0.1, 0.1, 0.2, 5, a, b0, b1, [0.5,0.5,ak.NaN]);} catch(e){return true;} return false;});
+   invalidArguments.add('non-number steps', function(){try{ak.fehlbergODE(Math.exp, 0.1, 0.1, 0.2, 5, a, b0, b1, c, 'a');} catch(e){return true;} return false;});
    invalidArguments.add('NaN x0', function(){try{ak.fehlbergODE(Math.exp, 0.1, 0.1, 0.2, 5, a, b0, b1, c)(ak.NaN, 1, 1);} catch(e){return true;} return false;});
    invalidArguments.add('NaN x1', function(){try{ak.fehlbergODE(Math.exp, 0.1, 0.1, 0.2, 5, a, b0, b1, c)(1, ak.NaN, 1);} catch(e){return true;} return false;});
    invalidArguments.add('infinite x0', function(){try{ak.fehlbergODE(Math.exp, 0.1, 0.1, 0.2, 5, a, b0, b1, c)(-ak.INFINITY, 0, 1);} catch(e){return true;} return false;});
@@ -77,6 +78,25 @@
     return true;
    }
 
+   function specificFirstOrder(ode, o) {
+    function f(x, y) {return -x*y}
+    var xn = 3;
+    var n = 30;
+    var dx = xn/n;
+    var e0 = Math.pow(dx, o)/10;
+    var e1 = Math.pow(dx, o)*10;
+    var i, xi;
+
+    ode = ode(f, dx, e0, e1);
+
+    for(i=0;i<n;++i) {
+     var xi = xn * i/n;
+     var yi = ode(0, xi, 1);
+     if(ak.diff(yi, Math.exp(-0.5*xi*xi))>10*e1) return false;
+    }
+    return true;
+   }
+
    function secondOrder(ode, o, a, b0, b1, c) {
     function f(t, x) {
      return ak.vector([x.at(1), -2*x.at(1) - 101*x.at(0)]);
@@ -98,20 +118,41 @@
     return true;
    }
 
+   function specificSecondOrder(ode, o) {
+    function f(t, x) {
+     return ak.vector([x.at(1), -2*x.at(1) - 101*x.at(0)]);
+    }
+    var tn = 2.5;
+    var n = 25;
+    var dt = tn/n;
+    var e0 = Math.pow(dt, o)/10;
+    var e1 = Math.pow(dt, o)*10;
+    var i, ti;
+
+    ode = ode(f, dt, e0, e1);
+
+    for(i=0;i<n;++i) {
+     var ti = tn * i/n;
+     var xi = ode(0, ti, ak.vector([1,0]));
+     if(ak.diff(xi.at(0), Math.exp(-ti)*(Math.cos(10*ti)+0.1*Math.sin(10*ti)))>10*e1) return false;
+    }
+    return true;
+   }
+
    evaluate.add('generic first order', function() {return firstOrder(ak.fehlbergODE, 5, a, b0, b1, c);});
    evaluate.add('generic first order default nodes', function() {return firstOrder(ak.fehlbergODE, 5, a, b0, b1);});
    evaluate.add('generic second order', function() {return secondOrder(ak.fehlbergODE, 5, a, b0, b1, c);});
    evaluate.add('generic second order default nodes', function() {return secondOrder(ak.fehlbergODE, 5, a, b0, b1);});
-   evaluate.add('heun rkf2 first order', function() {return firstOrder(ak.heunRKF2ODE, 2);});
-   evaluate.add('heun rkf2 second order', function() {return secondOrder(ak.heunRKF2ODE, 2);});
-   evaluate.add('bogacki-shampine rkf3 first order', function() {return firstOrder(ak.bogackiShampineRKF3ODE, 3);});
-   evaluate.add('bogacki-shampine rkf3 second order', function() {return secondOrder(ak.bogackiShampineRKF3ODE, 3);});
-   evaluate.add('fehlberg rkf5 first order', function() {return firstOrder(ak.fehlbergRKF5ODE, 5);});
-   evaluate.add('fehlberg rkf5 second order', function() {return secondOrder(ak.fehlbergRKF5ODE, 5);});
-   evaluate.add('cash-karp rkf5 first order', function() {return firstOrder(ak.cashKarpRKF5ODE, 5);});
-   evaluate.add('cash-karp rkf5 second order', function() {return secondOrder(ak.cashKarpRKF5ODE, 5);});
-   evaluate.add('dormand-prince rkf5 first order', function() {return firstOrder(ak.dormandPrinceRKF5ODE, 5);});
-   evaluate.add('dormand-prince rkf5 second order', function() {return secondOrder(ak.dormandPrinceRKF5ODE, 5);});
+   evaluate.add('heun rkf2 first order', function() {return specificFirstOrder(ak.heunRKF2ODE, 2);});
+   evaluate.add('heun rkf2 second order', function() {return specificSecondOrder(ak.heunRKF2ODE, 2);});
+   evaluate.add('bogacki-shampine rkf3 first order', function() {return specificFirstOrder(ak.bogackiShampineRKF3ODE, 3);});
+   evaluate.add('bogacki-shampine rkf3 second order', function() {return specificSecondOrder(ak.bogackiShampineRKF3ODE, 3);});
+   evaluate.add('fehlberg rkf5 first order', function() {return specificFirstOrder(ak.fehlbergRKF5ODE, 5);});
+   evaluate.add('fehlberg rkf5 second order', function() {return specificSecondOrder(ak.fehlbergRKF5ODE, 5);});
+   evaluate.add('cash-karp rkf5 first order', function() {return specificFirstOrder(ak.cashKarpRKF5ODE, 5);});
+   evaluate.add('cash-karp rkf5 second order', function() {return specificSecondOrder(ak.cashKarpRKF5ODE, 5);});
+   evaluate.add('dormand-prince rkf5 first order', function() {return specificFirstOrder(ak.dormandPrinceRKF5ODE, 5);});
+   evaluate.add('dormand-prince rkf5 second order', function() {return specificSecondOrder(ak.dormandPrinceRKF5ODE, 5);});
 
    fehlbergODE.add(invalidArguments);
    fehlbergODE.add(evaluate);
