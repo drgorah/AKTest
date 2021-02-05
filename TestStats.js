9f930006e3c8f97d728040de73793ea12b80e32e
ak <ak@thusspakeak.com>
2021-02-05 13:40:14 +0000

    renaming student t-test

--- TestStats.js
+++ TestStats.js
@@ -3,6 +3,6 @@
 (function() {
  var all = [];
  all.push('TestStats/TestChiSquaredTest.js');
- all.push('TestStats/TestStudentTTest.js');
+ all.push('TestStats/TestStudentsTTest.js');
  ak.using(all);
 })();
